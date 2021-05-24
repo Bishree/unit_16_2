d13e758e11eeefc114e21571269edb5a54b4b832
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-05-24 03:25:58 +0300

    Sending Multiple Values and Seeing the Receiver Waiting

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread")
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for receiver in rx {
+        println!("Got; {}", receiver);
+    }
+
+}
